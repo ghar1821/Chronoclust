a08d9a766e1bc0602cd483098c5224101b62c324
Givanna Putri <ghar1821@sydney.edu.au>
2019-08-05 14:19:37 +1000

    Added setup script

---
+++ chronoclust/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='chronoclust',
+    version='0.1',
+    packages=[''],
+    url='https://ghar1821.github.io/Chronoclust/',
+    license='GPL-3.0',
+    author='Givanna Putri',
+    author_email='ghar1821@sydney.edu.au',
+    description='A clustering algorithm that will perform clustering on each of a time-series of discrete datasets, and explicitly track the evolution of clusters over time.'
+)
