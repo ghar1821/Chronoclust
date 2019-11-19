79b59fa2aa0a9562a5c424417c1da359fd02f9dd
Givanna Putri <ghar1821@uni.sydney.edu.au>
2019-11-19 12:13:05 +1100

    Added sample result file

--- sample_run_script/sample_run_from_raw.py
+++ sample_run_script/sample_run_from_raw.py
@@ -1,12 +1,13 @@
 from main.main import run
+from pathlib import Path
 
-input_xml = '/Users/givanna/input.xml'
-config_xml = '/Users/givanna/config.xml'
-out = '/Users/givanna/out'
+home_dir = Path("/Users/givanna/workdir")
+input_xml = home_dir / Path("input.xml")
+config_xml = home_dir / Path("config.xml")
+out = home_dir / Path("output")
 gating = None
 prog = None
 
-
 run(config_xml=config_xml, input_xml=input_xml, output_dir=out, log_dir=out, gating_file=gating, program_state_dir=prog,
-    sort_output=True, normalise=False)
+    sort_output=True, normalise=True)
 
